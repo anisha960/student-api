9c77bc2fc8bce870e2397e8d42ad29874b58a64b
anisha960 <anishakashyap15@gmail.com>
2023-04-29 13:24:55 +0530

    try, catch

--- src/app.js
+++ src/app.js
@@ -10,15 +10,29 @@ app.use(express.json())
 
 
 //create a new student 
-app.post("/students", (req, res) => {
-    console.log(req.body)
-    const user = new Student(req.body)
+// app.post("/students", (req, res) => {
+//     console.log(req.body)
+//     const user = new Student(req.body)
 
-    user.save().then(() => {
-        res.status(201).send(user);
-    }).catch((err) => {
-        res.status(400).send(err)
-    })
+//     user.save().then(() => {
+//         res.status(201).send(user);
+//     }).catch((err) => {
+//         res.status(400).send(err)
+//     })
+// })
+
+
+// second way to create a new student
+app.post("/students", async(req, res) => {
+    const user = new Student(req.body)
+    
+    try{
+        const createUser = await user.save();
+    res.status(201).send(createUser);
+
+    }catch(e){
+        res.status(400).send(e)
+    }
 })
 
 app.listen(port, (req, res)=>{
